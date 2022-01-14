4545e9d40f1a316f4cb5497e3a7ea8aab373b95c
yusjuani <veroyusjuani@gmail.com>
2022-01-14 13:23:22 -0300

    componentes

---
+++ src/componentes/Cocina.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Cocina = () => {
+    return (
+      <div>
+        <h2>Cocina</h2>
+        <ul>
+          <li>articulo uno</li>
+          <li>articulo dos</li>
+          <li>articulo tres</li>
+        </ul>
+      </div>
+    )
+}
+
---
+++ src/componentes/Garzon.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+ export const Garzon = () => {
+    return (
+      <div>
+        <h2> Garzón</h2>
+        <ul>
+          <li>articulo uno</li>
+          <li>articulo dos</li>
+          <li>articulo tres</li>
+        </ul>
+      </div>
+    )
+}
+
+
