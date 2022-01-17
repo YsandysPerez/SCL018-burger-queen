6191f437eb6c39f19e234ae8a182ee802e0a7535
yusjuani <veroyusjuani@gmail.com>
2022-01-17 19:48:12 -0300

    detalles color

--- src/componentes/Inicio.js
+++ src/componentes/Inicio.js
@@ -10,8 +10,8 @@ export const Inicio = () => {
       <img src={logo} alt="" className="h-80 px-40"/>
       </div>
       <div className="flex justify-center space-x-10">
-      <Link to="/Garzon"><button className="h-20 px-20 font-semibold text-lg text-teal-50 transition-colors duration-150 bg-yellow-500 rounded-lg focus:shadow-outline hover:bg-teal-800">Mesero</button></Link>
-      <Link to="/Cocina"><button className="h-20 px-20 font-semibold text-lg text-teal-50 transition-colors duration-150 bg-yellow-500 rounded-lg focus:shadow-outline hover:bg-teal-800">Cocina</button></Link>
+      <Link to="/Garzon"><button className="h-20 px-20 font-semibold text-lg text-teal-50 transition-colors duration-150 bg-yellow-500 rounded-lg focus:shadow-outline hover:bg-yellow-600">Mesero</button></Link>
+      <Link to="/Cocina"><button className="h-20 px-20 font-semibold text-lg text-teal-50 transition-colors duration-150 bg-yellow-500 rounded-lg focus:shadow-outline hover:bg-yellow-600">Cocina</button></Link>
       </div>
     </div>
    </>
