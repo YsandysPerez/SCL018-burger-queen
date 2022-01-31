8b4ca07998754d08793dc8c8fe38e9579880c7e8
yusjuani <veroyusjuani@gmail.com>
2022-01-31 10:48:08 -0300

    bootstrap

--- src/componentes/Drinks.js
+++ src/componentes/Drinks.js
@@ -9,17 +9,17 @@ const Drinks = () => {
     return ( 
     <>
     {show === true ?
-    <div className=" ">
-        <button className="h-20 my-6  mx-2 px-20 font-semibold text-lg text-teal-50 transition-colors duration-150 bg-yellow-500 rounded-lg focus:shadow-outline hover:bg-yellow-600" onClick={() => showMenu(false)}>Bebidas</button>
+    <div >
+        <button class="btn btn-info btn-lg" onClick={() => showMenu(false)}>Bebidas</button>
     </div>
     :
-    <div className=" w-full">
-    <button className="h-20 my-6 mx-2 px-20 font-semibold text-lg text-teal-50 transition-colors duration-150 bg-yellow-500 rounded-lg focus:shadow-outline hover:bg-yellow-600" onClick={() => showMenu(true)}>Bebidas</button>
-    <div className=" ">
+    <div>
+    <button class="btn btn-info btn-lg" onClick={() => showMenu(true)}>Bebidas</button>
+    <div >
     {drink.map((element, index) => {
         return(
-        <div key={index} className= "w-1/2 p-1 ">
-        <button className=" m-2 w-1/5 h-12 bg-yellow-500 text-teal-50 rounded-lg transition-colors duration-150 focus:shadow-outline hover:bg-yellow-600 " onClick={() => allContext.addProduct(element)}>{element.name} ${element.price}</button>
+        <div key={index}>
+        <button class="btn btn-info btn-lg" onClick={() => allContext.addProduct(element)}>{element.name} ${element.price}</button>
         </div>
             );
         })}
