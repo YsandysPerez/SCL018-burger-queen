d2e5b126bf48739f787871f0e906c0b351be7b96
yusjuani <veroyusjuani@gmail.com>
2022-02-08 11:44:58 -0300

    css

--- src/componentes/Inicio.jsx
+++ src/componentes/Inicio.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import logo from "./logo.png";
+import {Button } from "react-bootstrap";
 
 export const Inicio = () => {
   return (
     <>
-    <div className="m-20">
-      <div className="flex justify-center">
-      <img src={logo} alt="" className="h-80 px-40"/>
+    <div class="back">
+      <div class="logo">
+      <img src={logo} alt="" />
       </div>
-      <div className="flex justify-center space-x-10">
-      <Link to="/Garzon"><button className="h-20 px-20 font-semibold text-lg text-teal-50 transition-colors duration-150 bg-yellow-500 rounded-lg focus:shadow-outline hover:bg-yellow-600">Garzón</button></Link>
-      <Link to="/Cocina"><button className="h-20 px-20 font-semibold text-lg text-teal-50 transition-colors duration-150 bg-yellow-500 rounded-lg focus:shadow-outline hover:bg-yellow-600">Cocina</button></Link>
+      <div class="botones">
+      <Link to="/Garzon"><Button  variant="warning"  >Garzón</Button> </Link>
+      <Link to="/Cocina"><Button variant="warning" >Cocina</Button></Link>
       </div>
     </div>
    </>
